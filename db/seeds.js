73543f1650eb9a5d00ad52bcc05105575742fdcf
Chris Hunter <orkem2@gmail.com>
2017-09-12 14:13:40 +0100

    created seeds, setup html

---
+++ db/seeds.js
@@ -0,0 +1,15 @@
+use farm;
+
+db.animals.insert([
+{
+  name: "Chongo",
+  type: "House Elf",
+  feedingTimes: [8,12,17]
+},
+{
+  name: "Taurus",
+  type: "Minotaur",
+  feedingTimes: [6,9,12,17]
+}
+  ])
+
